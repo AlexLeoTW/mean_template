b623ee46324d7b33b86840cc038e28e09a234f4c
AlexLeo <alex@alexleo.click>
2017-07-12 17:50:51 +0800

    add "ExampleService"

--- public/application.js
+++ public/application.js
@@ -1,12 +1,17 @@
 const mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'users', 'example']);
 
 mainApplicationModule.config(['$locationProvider',
   function($locationProvider) {
     $locationProvider.hashPrefix('!');
   }
 ]);
+
+// Facebook redirect hack
+// if (window.location.hash === '#_=_') {
+//   window.location.hash = '#!';
+// }
 
 angular.element(document).ready(function() {
   angular.bootstrap(document, [mainApplicationModuleName]);
